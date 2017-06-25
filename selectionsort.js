f2630e686a6ed06ef3f157ee5307d42e97660283
BK <bkam03@yahoo.com>
2017-06-24 18:49:15 -1000

    selectionsort sorts

---
+++ selectionsort.js
@@ -0,0 +1,31 @@
+function selectionSort ( array ){
+  var currentMinimum = 0;
+  var currentMinimumPosition = 0;
+
+  var numberHolder = null;
+
+  for( var passNumber = 0; passNumber < array.length - 1; passNumber++ ){
+    currentMinimum = array[ passNumber ];
+    currentMinimumPosition = passNumber;
+
+    for( var index = passNumber + 1; index < array.length; index++ ){
+      if( array[ index ] < currentMinimum ){
+        currentMinimum = array[ index ];
+        currentMinimumPosition = index;
+      }
+    }
+    if( currentMinimumPosition > passNumber ){
+      numberHolder = array[ passNumber ];
+      array[ passNumber ] = currentMinimum;
+      array[ currentMinimumPosition ] = numberHolder;
+    }
+  }
+
+  return array;
+
+}
+
+var array = [19, 97, 76, 98, 35, 29, 71, 27, 65, 86 ];
+
+var sorted = selectionSort( array );
+console.log( sorted );
